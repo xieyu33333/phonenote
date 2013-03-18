8e7995bbbfc4421323b27a9f8d71e16aa4a78ee1
xieyu33333 <xieyu33333@gmail.com>
2013-03-18 23:21:26 +0800

    update helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,12 @@
 module ApplicationHelper
   include SessionsHelper
+  
+  def active_navigate(column_name, &block)
+    if column_name == @navigate_id
+      content_tag(:li, :class => 'active', &block)
+    else
+      content_tag(:li, &block)
+    end
+  end
+  
 end
